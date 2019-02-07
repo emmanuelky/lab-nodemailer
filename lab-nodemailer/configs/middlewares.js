c2d133b905e6bf233b36fcb6687e3a0ebd8d8800
Emmanuel Akhere Iyere <iyereemmanuelakhere@Emmanuels-MacBook-Pro.local>
2019-02-07 17:55:27 +0100

    done

--- lab-nodemailer/configs/middlewares.js
+++ lab-nodemailer/configs/middlewares.js
@@ -1,7 +1,9 @@
 module.exports = {
-  checkConnectedAndActive: function(req, res, next) {
+  checkConnectedAndActive: function(req,res,next) {
     // If the user is connected and its status is active, go to the next middleware
-    if (req.user && req.user.status === "Active") next();
-    else res.redirect("/");
+    if (req.user && req.user.status === 'Active')
+      next()
+    else 
+      res.redirect('/')
   }
-};
+}
